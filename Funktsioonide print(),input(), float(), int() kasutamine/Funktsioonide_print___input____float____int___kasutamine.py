8ed984039cb9ca2f087029132ed621dc49583b05
Anton Ivanov <anton90322@gmail.com>
2024-01-17 18:51:48 +0200

    Добавьте файлы проекта.

---
+++ Funktsioonide print(),input(), float(), int() kasutamine/Funktsioonide_print___input____float____int___kasutamine.py
@@ -0,0 +1,29 @@
+﻿
+# #1
+# st="Tere, maailm!"
+# print(st)
+# a = input("Palun siseda oma nimi")
+# print(st + " Tervitan sind " + a)
+# vanus = input("Palun siseda ome vanus")
+# print(st + " Tervitan sind " + a + "! Sa oled " + vanus + " aastat vana.")
+
+# #2
+# vanus = 18 #int
+# eesnimi = "Jaak" #str
+# pikkus = 16.5 #float
+# kas_käib_koolis = True #bool
+# #kood tüüpide kontrollimiseks
+# print("Тип переменной:", type(input("Выберите переменную. ")))
+
+# #3
+# k = 32
+# print("Laual on " +str(k)+ " kommid.")
+# print(k-int(input("Mitu kommi sa soovid laualt ara votta? ")))
+
+# #4
+# def diameter(tree):
+#     diameter = tree / 3.14
+#     return diameter
+# tree = float(input("Введите длинну окружности дерева (в см) : "))
+# tree_diameter = round(diameter(tree), 2)
+# print("Диаметр равен "+(str(tree_diameter))+" см.")
