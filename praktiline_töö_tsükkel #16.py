1b12bf037fe30cdcb951efbca1fed449fcf73368
Antonlvanov <156616115+Antonlvanov@users.noreply.github.com>
2024-01-31 16:06:27 +0200

    Update praktiline_töö_tsükkel #16.py

--- praktiline_töö_tsükkel #16.py
+++ praktiline_töö_tsükkel #16.py
@@ -1,16 +1,20 @@
 ﻿import random
+# Genereerime arvu 1 kuni 100
 arvuti_arv = random.randint(1, 100) 
 while True:
-    sisend = input("Sisesta oma arv: ")
-    try:
-        kasutaja_arv = int(sisend)
+    # Kasutaja arvab arvu ära
+    sisend = input("Sisesta oma arv: ") 
+    # Sisendi tüüpi kontroll
+    try: 
+        kasutaja_arv = int(sisend) 
     except ValueError:
         print("Palun sisesta ainult arvud.")
         continue
-    if kasutaja_arv == arvuti_arv:
+    # Kahe saadud arvu võrdlus
+    if kasutaja_arv == arvuti_arv: 
         print("Õige arv! Palju õnne, võitsid mängu!")
         break
-    elif kasutaja_arv < arvuti_arv:
+    elif kasutaja_arv < arvuti_arv: 
         print("Proovi suuremat arvu.")
     else:
         print("Proovi väiksemat arvu.")
