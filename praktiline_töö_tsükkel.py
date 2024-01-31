c8090eebcbe777569145a315653d64a57fda6eca
Antonlvanov <156616115+Antonlvanov@users.noreply.github.com>
2024-01-31 09:40:33 +0200

    Add files via upload

---
+++ praktiline_töö_tsükkel.py
@@ -0,0 +1,18 @@
+﻿import random
+arvuti_arv = random.randint(1, 100) 
+while True:
+    sisend = input("Sisesta oma arv: ")
+    try:
+        kasutaja_arv = int(sisend)
+    except ValueError:
+        print("Palun sisesta ainult arvud.")
+        continue
+    if kasutaja_arv == arvuti_arv:
+        print("Õige arv! Palju õnne, võitsid mängu!")
+        break
+    elif kasutaja_arv < arvuti_arv:
+        print("Proovi suuremat arvu.")
+    else:
+        print("Proovi väiksemat arvu.")
+
+
