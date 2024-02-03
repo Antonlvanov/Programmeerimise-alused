862f0cb2064959a9ad03a144479ee50f688fcb1b
Antonlvanov <156616115+Antonlvanov@users.noreply.github.com>
2024-02-03 23:19:52 +0200

    Delete Listid/tt.py

--- Listid/tt.py
+++
@@ -1,8 +0,0 @@
-﻿
-glas = list("aeiouõäöüAEIOUÕÄÖÜаеёиоуыэюяАЕЁИОУЫЭЮЯ")
-sogl = list("bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZбвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ")
-punctuation = list(',.!?;:()[]{}\'"-_/\\|@#$%^&*+=<>~')
-spaces=list(" ")
-print(glas)
-print(sogl)
-print(punctuation)
