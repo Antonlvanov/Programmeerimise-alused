1a493b7581cd3616207f9e832814ef1a3cc99b70
Anton Ivanov <anton90322@gmail.com>
2024-02-05 01:08:36 +0200

    sdasd

--- Listid/Listid.py
+++ Listid/Listid.py
@@ -1,31 +1,31 @@
-﻿#1
+﻿# #1
 
-glas = list("aeiouõäöüAEIOUÕÄÖÜаеёиоуыэюяАЕЁИОУЫЭЮЯ")
-sogl = list("bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZбвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ")
-sumbolid = list(",.!?;:-—–()[]{}\"'")
-spaces=list(" ")
-while True:
-    sisend=str(input("Palun Sisestage sõna või lause /n>> "))
+# glas = list("aeiouõäöüAEIOUÕÄÖÜаеёиоуыэюяАЕЁИОУЫЭЮЯ")
+# sogl = list("bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZбвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ")
+# sumbolid = list(",.!?;:-—–()[]{}\"'")
+# spaces=list(" ")
+# while True:
+#     sisend=str(input("Palun Sisestage sõna või lause /n>> "))
     
-    space = 0
-    g_arv = 0
-    s_arv = 0
-    sumb = 0
+#     space = 0
+#     g_arv = 0
+#     s_arv = 0
+#     sumb = 0
     
-    for char in sisend:
-        if char in glas:
-            g_arv += 1
-        elif char in sogl:
-            s_arv += 1
-        elif char in sumbolid:
-            sumb += 1
-        elif char in spaces:
-            space += 1
+#     for char in sisend:
+#         if char in glas:
+#             g_arv += 1
+#         elif char in sogl:
+#             s_arv += 1
+#         elif char in sumbolid:
+#             sumb += 1
+#         elif char in spaces:
+#             space += 1
         
-    print(f"Vokaalid: {g_arv}")
-    print(f"Konsonandid: {s_arv}")
-    print(f"Sümbolid: {sumb}")
-    print(f"Tühikud: {space}")
+#     print(f"Vokaalid: {g_arv}")
+#     print(f"Konsonandid: {s_arv}")
+#     print(f"Sümbolid: {sumb}")
+#     print(f"Tühikud: {space}")
 
 
 #2
@@ -65,14 +65,17 @@ while True:
         if change_name in names:
             index = names.index(change_name)
             new_name = input("Sisestage uus nimi: ")
-            names[index] = new_name
-            print("Nimi muudetud edukalt.")
-
+            if new_name not in names:
+                names[index] = new_name
+            else:
+                print("See nimi on juba sisestatud. Palun sisestage teine nimi.")
+                continue
+            
             names.sort()
             print("\nUuendatud nimede ja vanuste nimekiri:")
             for i in range(len(names)):
                 print(f"{names[i]} - Vanus: {ages[i]} aastat")
-
+            
             print("\nVanuste statistika:")
             print("Suurim vanus:", max_age)
             print("Väikseim vanus:", min_age)
@@ -81,6 +84,6 @@ while True:
         else:
             print("Sellist nime ei leitud. Palun sisestage olemasolev nimi.")
     else:
-        exit(0)
+        continue
 
     
