eece3367ee0eeee4b122dc1ebe62cf4c30cc7b6e
Anton Ivanov <anton90322@gmail.com>
2024-02-21 15:22:36 +0200

    s

---
+++ UserFunkts/UserFunkts.py
@@ -0,0 +1,7 @@
+from module1 import *
+
+s=(float(input("arv 1: ")),float(input("arv 2: ")),str(input("Tehe (+,-,*,:): ")))
+s=arithmetic(*s)
+print(s)
+print(checktype(s))
+
---
+++ UserFunkts/module1.py
@@ -0,0 +1,20 @@
+def arithmetic(arv1:float,arv2:float,tehe:str)->any:
+    try:
+        if tehe == "+":
+            tulemus = arv1 + arv2
+        elif tehe == "-":
+            tulemus = arv1 - arv2
+        elif tehe == "*":
+            tulemus = arv1 * arv2
+        elif tehe == "/":
+            if arv2 != 0:
+                tulemus = arv1 / arv2
+            else:
+                print("nulliga jagamine ei ole lubatud.")
+                exit(0)
+        return float(tulemus)   
+    except: return("Vigane tehe")
+
+def checktype(x)->any:
+    s=type(x)
+    return s  
