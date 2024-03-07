85298681c9e0a775fdcf48d786804cfd0c325f62
Antonlvanov <156616115+Antonlvanov@users.noreply.github.com>
2024-03-07 15:52:21 +0200

    Add files via upload

---
+++ Registreerimine ja autoriseerimine/Registreerimine_ja_autoriseerimine.py
@@ -0,0 +1,34 @@
+﻿from module1 import *
+
+kasutajad=["A","G","C","D","E"]
+paroolid=[]
+for i in range(len(kasutajad)):
+    k=''
+    paroolid.append(salasona_genereerimine(k))
+while True:
+    print("\n1 Registreetimine\n2 Autoriseerimine\n3 Parooli taastamine\n4 Kinnita")
+    print()
+    try:
+        v=int(input())
+    except: 
+        print("Vale andmed")
+    if 0<=v<7:
+        if v==1:
+            registreerimine(kasutajad,paroolid)
+        elif v==3:
+            unustanud_parooli_taastamine(kasutajad,paroolid)
+        elif v==4:
+            exit(0)
+        elif v==2:
+            while autoriseerimine(kasutajad,paroolid):
+                print("\n1 Unustanud parooli taastamine\n2 Nimi või parooli muutumine")
+                c=int(input())
+                if c==1:
+                    unustanud_parooli_taastamine(kasutajad,paroolid)
+                    continue
+                if c==2:
+                    nime_voi_parooli_muutmine(kasutajad,paroolid)
+                    continue
+    else: 
+        print("Vale number")
+        continue
---
+++ Registreerimine ja autoriseerimine/module1.py
@@ -0,0 +1,74 @@
+﻿import string
+import random
+
+def salasona_genereerimine(k:str):
+    for i in range(12):
+        t=random.choice(string.ascii_letters) 
+        num=random.choice([1,2,3,4,5,6,7,8,9,0])
+        t_num=[t,str(num)]
+        k+=random.choice(t_num)
+    k+=random.choice("""`~!@#$%^&*()_-+={[}}|\:;"'<,>.?/""")
+    return k
+
+def registreerimine(kasutajad:list, paroolid:list)->any:
+    try: nimi=str(input("Sisesta soovitud nimi: ")).capitalize()
+    except: "Vale sisend"
+    kasutajad.append(nimi)
+    val=int(input("Valige kas te soovite automaatne parooli genereerimine (1) või luua parooli ise (2): "))
+    parool=str()
+    if val==1:
+        parool=salasona_genereerimine(parool)
+        paroolid.append(parool)
+        print(f"{nimi}! Genereeritud salasõna on {parool}")
+        print("Konto edukalt registreeritud!")
+        return
+    if val==2:
+        paroolid.append(input("Sisesta soovitud salasõna: "))
+        print("Konto edukalt registreeritud!")
+        return
+    else:
+        print("Vale valik")
+
+def autoriseerimine(kasutajad:list, paroolid:list)->any:
+    try: kasutajanimi=(str(input("Kasutaja nimi: "))).capitalize()
+    except: print("Vale sisend")
+    if kasutajanimi in kasutajad:
+        try: parool=(str(input("Salasõna: ")))
+        except: print("Vale sisend")
+        if paroolid[kasutajad.index(kasutajanimi)]==parool:
+            print("Sisselogimine on edukalt!")
+            return True
+        else: print("Vale salasõna")
+        return False
+    else: print("Kasutaja ei leidnud")
+    return False
+    
+def unustanud_parooli_taastamine(kasutajad:list, paroolid:list)->any:
+    try: kasutajanimi=(str(input("Kasutaja nimi: "))).capitalize()
+    except: print("Vale sisend")
+    if kasutajanimi in kasutajad:
+        print("Teie parool on: ",paroolid[kasutajad.index(kasutajanimi)])
+    else: print("Kasutaja ei leidnud.")
+
+def nime_voi_parooli_muutmine(kasutajad:list, paroolid:list)->any:
+    s=int(input("Kas tahate muutuda nimi(1) või salasõna(2)? "))
+    if s==1:
+        try: kasutajanimi=(str(input("Uus nimi: "))).capitalize()
+        except: print("Vale sisend")
+        kasutajad[kasutajad.index(kasutajanimi)]==kasutajanimi
+        print("Kasutaja nimi oli muudatud")
+    if s==2:
+        try: kasutajanimi=(str(input("Sisesta oma kasutajanimi: "))).capitalize()
+        except: print("Vale sisend")
+        try: uusparool=(str(input("Sisesta uus salasõna: ")))
+        except: print("Vale sisend")
+        spec="""`~!@#$%^&*()_-+={[}}|\:;"'<,>.?/"""
+        if uusparool.isdigit() and uusparool.isupper() and uusparool.islower() and any(c in spec for c in s):
+            paroolid[kasutajad.index(kasutajanimi)]
+            print("Salasõna oli muudatud")
+        else: print("Parool peab siselda numbrid, tähed põhjastähti, suurtähti ja spetsialist tähed.")
+    else:
+        print("Vale valik")
+
+
+#def näita_koik_kasutajad:
