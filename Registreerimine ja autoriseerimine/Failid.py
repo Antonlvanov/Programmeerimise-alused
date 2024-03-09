44f26d2c207595b8ff1a0cdd6359c1d34f6fbb95
Anton Ivanov <anton90322@gmail.com>
2024-03-09 22:09:01 +0200

    emailid

---
+++ Registreerimine ja autoriseerimine/Failid.py
@@ -0,0 +1,38 @@
+﻿from module1 import *
+from os import system
+from gtts import *
+import pandas as pd
+
+def template():
+    template_df = pd.DataFrame({
+        'Username': [],
+        'Email': [],
+        'Phone': [],
+        'Address': []
+    })
+    template_df.to_csv('users_template.csv', index=False)
+    
+def loe_failist(kasutaja_info:str,kasutajad:list,paroolid:list,emailid:list)->any:
+    """Loeme failist ja salvestame järjendisse. Funktsioon tagastab järjend
+    :param str fail:
+    :rtype: list
+    """
+    try:
+        f=open(fail,'r',endcoding="utf-8") #tryw 
+        jarjend=[]
+        for rida in f:
+            jarjend.append(rida.strip())
+        f.close()
+        return jarjend
+    except Exception as e:
+        print(e)
+
+def kirjuta_failisse(fail:str,kasutajad:list,paroolid:list,emailid:list):
+    """
+    """
+    for i in range(len(jarjend)):
+        jarjend.append(input(f"{i+1} element: "))
+    f=open(fail,'w',encoding="utf-8")
+    for el in jarjend:
+        f.write(el+"\n")
+    f.close()
---
+++ Registreerimine ja autoriseerimine/Registreerimine_ja_autoriseerimine.py
@@ -0,0 +1,42 @@
+﻿from module1 import *
+from Failid import *
+
+kasutajad=["Anna","Grisha","Sasha","Daniil","Eren"]
+paroolid=[]
+emailid=[]
+template()
+for i in range(len(kasutajad)):
+    k=''
+    paroolid.append(salasona_genereerimine(k))
+    emailid.append(str(kasutajad[i])+"@tthk.ee")
+
+while True:
+    print("\n1 Registreetimine \n2 Autoriseerimine \n3 Parooli taastamine\n4 Kinnita")
+    print()
+    v=0
+    try:
+        v=int(input())
+    except: 
+        print("Vale andmed")
+    if 0<=v<7:
+        if v==1:
+            registreerimine(kasutajad,paroolid,emailid)
+        elif v==3:
+            unustanud_parooli_taastamine(kasutajad,paroolid,emailid)
+        elif v==4:
+            exit(0)
+        elif v==2:
+            re,kasutajanimi=autoriseerimine(kasutajad,paroolid)
+            if re==True:
+                print("\n1 Unustanud parooli taastamine\n2 Näita kõik kasutajad andmeid")
+                c=int(input())
+                if c==1:
+                    oma_nimi_voi_parooli_muutmine(kasutajad,paroolid,kasutajanimi)
+                    continue
+                if c==2:
+                    näita_koik_kasutajad_andmeid(kasutajad,paroolid)
+                    continue
+            else: continue
+    else: 
+        print("Vale number")
+        continue
